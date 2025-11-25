ff57c5f10bc7f72cb51973a0f3616922adf9f2e5
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-25 14:28:00 -0300

    criacao da entity membro

---
+++ portfolioApi/src/main/java/com/jtarcio/portfolioapi/model/entity/Membro.java
@@ -0,0 +1,29 @@
+package com.jtarcio.portfolioapi.model.entity;
+
+import com.jtarcio.portfolioapi.model.entity.enums.Atribuicao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Table(name = "tb_membros")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Membro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @NonNull
+    private Atribuicao atribuicao;
+
+    //variavel projeto aqui.
+}
