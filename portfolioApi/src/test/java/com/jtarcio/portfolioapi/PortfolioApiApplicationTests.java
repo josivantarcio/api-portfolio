00671baaa50648245964abbacd16d184780b2d92
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-25 12:32:31 -0300

    Adiciona pasta portfolioApi

---
+++ portfolioApi/src/test/java/com/jtarcio/portfolioapi/PortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtarcio.portfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
